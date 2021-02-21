944b5e7dce4a9d943ff8b104d218d819fc5f8860
Christopher Prince <chris@SpasticMuffin.biz>
2021-02-21 00:39:08 -0700

    version 0.7

--- Sources/iOSBasics/API/Networking/Reachability.swift
+++ Sources/iOSBasics/API/Networking/Reachability.swift
@@ -18,6 +18,7 @@ class Reachability: ObservableObject {
     private var cancellable: AnyCancellable!
     
     init() {
+#if DEBUG
         // See also https://stackoverflow.com/questions/27500940#if DEBUG
         if ProcessInfo.processInfo.environment["XCTestConfigurationFilePath"] != nil {
             isReachable = true
